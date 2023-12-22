4cf4cba48f854df4162d2ef39a6215f6c62c54f3
UtkarshVerma-Space <utkarshverma001@outlook.com>
2023-12-22 14:59:29 +0530

    addded payment

--- app/alet/alert.jsx
+++ app/alet/alert.jsx
@@ -7,7 +7,7 @@ export default function Alert() {
 
   React.useEffect(() => {
     onOpen(); // Open the modal when the component mounts
-  }, []); // Empty dependency array ensures the effect runs once when the component mounts
+  }, [onOpen]); // Empty dependency array ensures the effect runs once when the component mounts
 
 
   return (
@@ -21,19 +21,19 @@ export default function Alert() {
         <ModalContent>
           {(onClose) => (
             <>
-              <ModalHeader className="flex flex-col gap-1">🚀 Printing Press at Full Throttle! 🚀 <b>High Demand Alert!</b></ModalHeader>
+              <ModalHeader className="flex flex-col gap-1">🚀 Printing Press at Full Throttle 🚀 <b>High Demand Alert</b></ModalHeader>
               <ModalBody>
                 <p> 
-                Our ink barrels are bursting, paper planes are grounded—our print party is on overload! 🎉 
+                Our ink barrels are bursting  paper planes are grounded—our print party is on overload! 🎉 
                 </p>
                 <p>
-                📢 Attention: <b>Due to high demand</b>, we can't take more orders right now. Existing orders might take up to 10 days for processing. We appreciate your patience and understanding. Thank you! 🙏
+                📢 Attention: <b>Due to high demand</b>,we can not take more orders right now. Existing orders might take up to 10 days for processing. We appreciate your patience and understanding. Thank you! 🙏
                 </p>
               </ModalBody>
               <ModalFooter>
 
                 <Button color="primary" onPress={onClose}>
-                  Okay, but I want to Place Urgent Order 👀 🙏🏼
+                  Okay. but I want to Place Urgent Order 👀 🙏🏼
                 </Button>
               </ModalFooter>
             </>
